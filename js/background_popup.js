053c2f1000db65ed94e5ae2898b936c488c57767
bottlesun <bottlesun3514@gmail.com>
2022-03-11 17:04:16 +0900

    backgroundpopup_update 22.03.11

--- js/background_popup.js
+++ js/background_popup.js
@@ -1,10 +1,21 @@
+const popupBg = document.querySelector('.popup_bg');
+const popupWrap = document.querySelector('.popup_wrap');
 
-        const popupBtn = document.querySelector('.popup_btn a');
-        const popupWrap = document.querySelector('.popup_wrap');
+const m_popupBtn = document.querySelectorAll('.popup_btn');
 
-        function ClickFade() {
-            popupWrap.classList.toggle('fadeOutAction');
-            setTimeout(() => {
-                popupWrap.style.display = "none";
-            }, 1100);
-        }
+m_popupBtn.forEach((v,i) => {
+    const popupList = document.querySelector('.popupmenu_'+[i]);
+
+    m_popupBtn[i].addEventListener('click',(e)=> {
+        e.preventDefault();
+        popupList.style.display = "none";
+    });
+
+});
+
+function ClickFade() {
+    popupWrap.classList.add('fadeOutAction');
+    setTimeout(() => {
+        popupWrap.style.display = "none";
+    }, 1000);
+}
